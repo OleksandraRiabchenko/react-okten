e11dec10d516d38ebb13ccf37316e585fb917053
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-06-25 20:51:04 +0300

    Practice1 user comments

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,26 +1,11 @@
-import {useState, useEffect} from 'react';
+import Comment from '../comment/Comment'
 
-export default function Comments() {
-    let [comments, setComments] = useState([]);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/comments')
-            .then(value => value.json())
-            .then(response => {
-                setComments(response);
-            })
-    }, [])
-
-
-    return (
-        <div>
-            {
-                comments.map(comment =>
-                    <div>
-                        {comment.id} - {comment.name} from {comment.email}
-                        <p>comment.body</p>
-                    </div>)
-            }
-        </div>
-    )
+export default function Comments({ items }) {
+  return (
+      <div>
+        {
+          items.map(value => <Comment item={value}/>)
+        }
+      </div>
+  )
 }
