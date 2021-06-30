2445a4d7660ef63733f77d6fdf49504426e7f4dd
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-06-30 14:11:36 +0300

    HW2 user posts Lesson2 axios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import {useEffect, useState} from 'react';
-import {getUsers, getUserPosts} from './services/Api';
+import './App.css'
+import { useEffect, useState } from 'react';
+import { getUsers, getUserPosts, getUser } from './services/Api';
 import Users from './components/users/Users';
-import Post from './components/post/Post'
+import Post from './components/post/Post';
+import User from './components/user/User'
 
 export default function App() {
-    // let [userPosts, setUserPosts] = useState(null)
-    // const showPosts = (id) => {
-    //     console.log(id);
-    //     getUserPosts(id).then(value => setUserPosts([...value.data]))
-    //     getUserPosts(id).then(value => console.log(value.data))
-    // }
+  let [users, setUsers] = useState([]);
 
-    let [users, setUsers] = useState([]);
-    useEffect (() => {
-        getUsers().then(value =>
-             setUsers([...value.data]))
-    }, [])
+  useEffect(() => {
+    getUsers().then(value => {
+      console.log(value.data)
+      return setUsers(value.data)
+    })
+  }, [])
 
+  let [user, setUser] = useState(null)
+  let [posts, setPosts] = useState(null);
+  const showPost = (id) => {
+    getUser(id).then(value => setUser(value.data))
+    getUserPosts(id).then(value => setPosts(value.data))
+  }
 
-    return (
-        <div>
-            <Users items={users} showPosts={showPosts}/>
-            {
-                // <Post/> && <Post/>
-            }
-            <Post/>
+  return (
+      <div className={'wrap'}>
+        <Users className={'main'} items={users} showPost={showPost}/>
+        <hr/>
+        <div className={'posts'}>
+          {
+            user && <h3>{`${JSON.stringify(user.name)} posts:`}</h3>
+          }
+          {
+            posts && <div>{posts.map(value => <Post key={value.id} item={value}/>)} </div>
+          }
         </div>
-    );
+      </div>
+
+  );
 }
-
-
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,14 +1,12 @@
-// import { useEffect, useState } from "react";
 import User from '../user/User';
 
-export default function Users({items, showPosts}) {
-
+export default function Users({items, showPost}) {
 
     return(
         <div>
-          {
-            items.map(value => <User key={value.id} item={value} showPosts={showPosts}/>)
-          }
+            {
+                items.map(value => <User key={value.id} item={value} showPost={showPost}/>)
+            }
         </div>
     )
 }
