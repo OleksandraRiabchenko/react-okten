f4d43996b3bb502d83227ab250224072bc568495
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-06-30 03:00:52 +0300

    HW2 user post Lesson2 axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
+import {useEffect, useState} from 'react';
+import {getUsers, getUserPosts} from './services/Api';
 import Users from './components/users/Users';
+import Post from './components/post/Post'
 
-function App() {
+export default function App() {
+    // let [userPosts, setUserPosts] = useState(null)
+    // const showPosts = (id) => {
+    //     console.log(id);
+    //     getUserPosts(id).then(value => setUserPosts([...value.data]))
+    //     getUserPosts(id).then(value => console.log(value.data))
+    // }
+
+    let [users, setUsers] = useState([]);
+    useEffect (() => {
+        getUsers().then(value =>
+             setUsers([...value.data]))
+    }, [])
+
+
     return (
         <div>
-            <Users/>
+            <Users items={users} showPosts={showPosts}/>
+            {
+                // <Post/> && <Post/>
+            }
+            <Post/>
         </div>
     );
 }
 
-export default App;
+
