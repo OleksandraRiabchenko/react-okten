ebf12a37a08852e56a8865277de8e01778f4d77f
OleksandraRiabchenko <79690371+OleksandraRiabchenko@users.noreply.github.com>
2021-07-01 13:58:39 +0300

    HW2 Lesson2 Post (axios) после замечаний

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css'
 import { useEffect, useState } from 'react';
 import { getUsers, getUserPosts, getUser } from './services/Api';
 import Users from './components/users/Users';
+import Posts from './components/posts/Posts'
 import Post from './components/post/Post';
 import User from './components/user/User'
 
 export default function App() {
-  let [users, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     getUsers().then(value => {
@@ -15,8 +16,8 @@ export default function App() {
     })
   }, [])
 
-  let [user, setUser] = useState(null)
-  let [posts, setPosts] = useState(null);
+  const [user, setUser] = useState(null)
+  const [posts, setPosts] = useState(null);
   const showPost = (id) => {
     getUser(id).then(value => setUser(value.data))
     getUserPosts(id).then(value => setPosts(value.data))
@@ -31,7 +32,7 @@ export default function App() {
             user && <h3>{`${user.name} posts:`}</h3>
           }
           {
-            posts && <div>{posts.map(value => <Post key={value.id} item={value}/>)} </div>
+            posts && <div><Posts items={posts}/> </div>
           }
         </div>
       </div>
